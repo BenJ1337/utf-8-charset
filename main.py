0eeac305ba6d9b4c387a92ca65c664a6c1053024
Benjamin Hacker <email@benjamin-hacker.de>
2021-06-26 15:33:34 +0200

    test

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
+num = 3000
 for files in range(1, 10):
-    with open("unicode_"+str(10000*files)+".md", "w") as file:
-        file.write("### ")
+    with open("unicode_"+str(num*files)+".md", "w") as file:
+        file.write("")
         start = 33
         print(files)
         if files != 1:
-            start = 10000*(files-1) + 1
+            start = num*(files-1) + 1
 
         line0 = "| "
         line1 = "| "
         line = "| "
         tmpCount = 0
-        for index in range(start, 10000*files):
+        for index in range(start, num*files):
             if(tmpCount < 10):
                 tmpCount += 1
                 #if index % 1000 == 0:
