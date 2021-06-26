edf058ccc28f3f1cded422e355850545dcce099d
Benjamin Hacker <email@benjamin-hacker.de>
2021-06-26 15:08:31 +0200

    update

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 for files in range(1, 10):
     with open("unicode_"+str(files)+".md", "w") as file:
-        for index in range(33, 1000*files):
-            out = '&#' + str(index) + "; = &#38;"
+        start = 33
+        if files != 1:
+            start = 10000*files + 1
+        for index in range(33, 10000*files):
+            out = '&#' + str(index) + "; = &#38;" + str(index)
             file.write(out)
